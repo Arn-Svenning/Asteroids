6a23b180ba1bdfaf3cd24ed8ec215a22f799ba65
Arn Svenning <arn@svenning.se>
2022-09-15 12:02:43 +0200

    Add project files.

---
+++ Asteroids/Animation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace Asteroids
+{
+    internal class Animation
+    {
+        Texture2D animation;
+        Rectangle sourceRectangle;
+        Vector2 position;
+
+        float elapsedTime;
+        float frameTime;
+        int numberOfFrames;
+        int currentFrame;
+        int width;
+        int height;
+        int frameWidth;
+        int frameHeight;
+        bool looping;
+
+        public Animation(ContentManager Content, string asset, float frameSpeed, int numberOfFrames, bool looping)
+        {
+            this.frameTime = frameSpeed;
+            this.numberOfFrames = numberOfFrames;
+            this.looping = looping;
+            this.animation = Content.Load<Texture2D>(asset);
+            frameWidth = (animation.Width / numberOfFrames);
+            frameHeight = (animation.Height);
+            position = new Vector2(0,0);
+        }
+
+        public void playAnimation(GameTime gameTime)
+        {
+            elapsedTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            sourceRectangle = new Rectangle(currentFrame * frameWidth ,0, frameWidth , frameHeight);
+
+            if (elapsedTime >= frameTime)
+            {
+                if (currentFrame >= numberOfFrames - 1)
+                {
+                    if (looping)
+                    {
+                        currentFrame = 0;
+                    }
+                    else
+                    {
+                        currentFrame++;
+                    }
+                    elapsedTime = 0;
+                }
+            }
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(animation, position, sourceRectangle, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+        }
+    }
+}
---
+++ Asteroids/Game1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Asteroids
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        Animation animation1;
+
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            animation1 = new Animation(Content, "spritesheet", 50f, 9, true);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            animation1.playAnimation(gameTime); 
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            animation1.draw(spriteBatch);
+
+            
+
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
